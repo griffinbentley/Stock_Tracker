bf9f71c3538c9828d381fe3eafbd7b11c487661e
Griffin Bentley <griffinbentley00@gmail.com>
2020-02-12 16:42:38 -0800

    Program stops if there are no stocks in workbook after window closes

--- Stock_Tracker.py
+++ Stock_Tracker.py
@@ -10,20 +10,25 @@ workbook_title = 'Stocks.xlsx'
 # runs window allowing for the user to add delete and change stocks and quantities
 win.main_window(workbook_title)
 
-# checks to see if a start date is needed
+# checks to see if there are any stocks in the workbook
 workbook = load_workbook(workbook_title, read_only=True)
 ws = workbook.active
-if ws['A3'].value is None:
-    stocks_q = wb.get_stocks(workbook_title)
-    stocks = list(stocks_q.keys())
-    win.init_date_win(workbook_title, stocks, stocks_q)
+if ws['B2'].value is not None:
 
-# checks to see if the user entered a start date or just closed the window
-workbook = load_workbook(workbook_title, read_only=True)
-ws = workbook.active
-if ws['A3'].value is not None:
-    # fills out any missing dates form the last date entered to the day before runtime
-    wb.fill_dates(workbook_title)
+    # checks to see if a start date is needed
+    if ws['A3'].value is None:
+        stocks_q = wb.get_stocks(workbook_title)
+        stocks = list(stocks_q.keys())
+        win.init_date_win(workbook_title, stocks, stocks_q)
 
-    # opens the workbook with all updates
-    os.startfile(workbook_title)
+    # checks to see if the user entered a start date or just closed the window
+    workbook = load_workbook(workbook_title, read_only=True)
+    ws = workbook.active
+
+    if ws['A3'].value is not None:
+
+        # fills out any missing dates form the last date entered to the day before runtime
+        wb.fill_dates(workbook_title)
+
+        # opens the workbook with all updates
+        os.startfile(workbook_title)
